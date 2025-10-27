956e4a623e2e1ae0e358c17f1c582dc985113029
Paul Cristian <paolocristiano24@icloud.com>
2025-10-27 11:17:23 +0200

    4

---
+++ src/main/java/com/example/University/Management/System/model/Teacher.java
@@ -0,0 +1,17 @@
+package com.example.University.Management.System.model;
+
+public class Teacher extends Staff {
+    private String title;
+    private String departmentId;
+
+    public Teacher(String id, String name, String phoneNumber, String title, String departmentId) {
+        super(id, name, phoneNumber);
+        this.title = title;
+        this.departmentId = departmentId;
+    }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDepartmentId() { return departmentId; }
+    public void setDepartmentId(String departmentId) { this.departmentId = departmentId; }
+}
