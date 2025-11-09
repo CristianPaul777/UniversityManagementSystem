83088cc7badf3cc4b536c6605be7671ea04e9c5e
Oros Dominic <dominicoros859@yahoo.com>
2025-11-09 16:12:34 +0200

    15

--- src/main/java/com/example/University/Management/System/repository/StaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.University.Management.System.repository;
-
-import com.example.University.Management.System.model.Staff;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class StaffRepository extends InMemoryRepository<Staff> {
-}
