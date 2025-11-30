9beeb5c91adb625ce1d44ea20d6c850be19581c2
Oros Dominic <dominicoros859@yahoo.com>
2025-11-30 22:53:55 +0200

    30

---
+++ src/main/java/com/example/University/Management/System/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.University.Management.System.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleGeneralException(Exception ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error/general";
+    }
+}
